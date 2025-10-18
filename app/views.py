cde8baa596e686a3dc7a8a86694c89e35f46bf86
Anna <jonyhoran@gmail.com>
2025-10-18 20:28:53 +0300

    Fixed views

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app.serializers import BookSerializer, PublisherSerializer, GenreSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+from rest_framework import status
 
 
 class BookList(APIView):
@@ -10,6 +11,13 @@ class BookList(APIView):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BookDetail(APIView):
@@ -24,12 +32,32 @@ class BookDetail(APIView):
         serializer = BookSerializer(book)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = BookSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class AuthorList(APIView):
     def get(self, request):
         authors = Author.objects.all()
         serializer = AuthorSerializer(authors, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AuthorDetail(APIView):
@@ -44,12 +72,32 @@ class AuthorDetail(APIView):
         serializer = AuthorSerializer(author)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = AuthorSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class PublisherList(APIView):
     def get(self, request, format=None):
         publishers = Publisher.objects.all()
         serializer = PublisherSerializer(publishers, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PublisherSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PublisherDetail(APIView):
@@ -64,12 +112,32 @@ class PublisherDetail(APIView):
         serializer = PublisherSerializer(publisher)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = PublisherSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class GenreList(APIView):
     def get(self, request, format=None):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
@@ -83,3 +151,16 @@ class GenreDetail(APIView):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = GenreSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -58,7 +58,6 @@ ROOT_URLCONF = 'bookstore.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:8000",
 ]
 
 TEMPLATES = [
